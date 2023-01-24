998f45d08b53a2f425af72aa30ca632f92a66284
ocsoares <casoares016@gmail.com>
2023-01-24 18:31:47 -0300

    build: enable environment variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,16 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { UserModule } from './user/user.module';
 
 @Module({
-    imports: [UserModule],
+    imports: [
+        ConfigModule.forRoot({
+            // Hablita para usar Variáveis de Ambiente (.env)
+            isGlobal: true, // Para TODO o Aplicativo (todos Controllers, Services, etc...)
+            envFilePath: '.env', // Nome do Arquivo .env
+        }),
+        UserModule,
+    ],
     controllers: [],
     providers: [],
 })
