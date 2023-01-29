9ed6ed05618feffe5739801e0ec28013c7eefce2
ocsoares <casoares016@gmail.com>
2023-01-28 21:07:00 -0300

    refactor: add comments to IUser

--- src/models/IUser.ts
+++ src/models/IUser.ts
@@ -1,5 +1,6 @@
-// OBS: O readonly NÃO impede do Objeto ser atualizado (update) no Banco de Dados,
-// porque ele impede apenas de ser atualizado no CÓDIGO, ou seja, em Desenvolvimento !!!
+// Readonly nas Props porque esse Objeto NÃO vai ser Modificado em nenhum momento no Código !!!
+// OBS: O readonly NÃO impede do Objeto ser modificado (update) no Banco de Dados,
+// porque ele impede apenas de ser modificado no CÓDIGO, ou seja, em Desenvolvimento !!!
 export interface IUser {
     readonly id?: string;
     readonly name: string;
