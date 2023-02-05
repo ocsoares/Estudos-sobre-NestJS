17a6a5e8bebaea87a70f26402040c2c007a8a78a
ocsoares <casoares016@gmail.com>
2023-02-05 16:59:44 -0300

    test: add test to the login user service

--- src/modules/user/use-cases/login-user/login-user.service.spec.ts
+++ src/modules/user/use-cases/login-user/login-user.service.spec.ts
@@ -1,9 +1,12 @@
 import { JwtService } from '@nestjs/jwt';
 import { Test, TestingModule } from '@nestjs/testing';
 import { LoginUserService } from '../login-user/login-user.service';
+import { IUserPayload } from './models/IUserPayload';
 
 describe('LoginUserService', () => {
     let service: LoginUserService;
+
+    const JWT = 'any_jwt';
 
     beforeEach(async () => {
         const module: TestingModule = await Test.createTestingModule({
@@ -13,16 +16,32 @@ describe('LoginUserService', () => {
                     provide: JwtService,
                     useExisting: JwtService, // Usa uma Instância EXISTENTE de JwtService (OBRIGATÓRIO) !!!
                     useValue: {
-                        sign: jest.fn(),
+                        sign: jest.fn().mockResolvedValue(JWT),
                     },
                 },
             ],
         }).compile();
 
         service = module.get<LoginUserService>(LoginUserService);
+
+        jest.spyOn(service, 'execute');
     });
 
     it('should be defined', () => {
         expect(service).toBeDefined();
+    });
+
+    it('should generate a JWT with a valid payload', async () => {
+        const payload: IUserPayload = {
+            sub: 'any_id',
+            name: 'Teste',
+            email: 'teste@gmail.com',
+        };
+
+        const result = await service.execute(payload);
+
+        expect(result).toEqual(JWT);
+        expect(service.execute).toHaveBeenCalledTimes(1);
+        expect(service.execute).toHaveBeenCalledWith(payload);
     });
 });
