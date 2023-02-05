b85a4e62e4babb87bb7aee9f24acf81aa5a027eb
ocsoares <casoares016@gmail.com>
2023-02-05 12:09:09 -0300

    test: add validation email test to the auth service test

--- src/modules/auth/auth.service.spec.ts
+++ src/modules/auth/auth.service.spec.ts
@@ -2,8 +2,17 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { UserRepository } from '../../repositories/abstracts/UserRepository';
 import { AuthService } from '../auth/auth.service';
 
+// FAZER TESTES...
 describe('AuthService', () => {
     let service: AuthService;
+    let repository: UserRepository;
+
+    type authUserData = { email: string; password: string };
+
+    const userData: authUserData = {
+        email: 'Teste',
+        password: 'teste123',
+    };
 
     beforeEach(async () => {
         const module: TestingModule = await Test.createTestingModule({
@@ -19,9 +28,24 @@ describe('AuthService', () => {
         }).compile();
 
         service = module.get<AuthService>(AuthService);
+        repository = module.get<UserRepository>(UserRepository);
+
+        jest.spyOn(service, 'validateUser');
     });
 
     it('should be defined', () => {
         expect(service).toBeDefined();
+        expect(repository).toBeDefined();
+    });
+
+    it('should NOT validate a user if email is invalid', async () => {
+        (repository.findByEmail as jest.Mock).mockResolvedValue(false);
+
+        await expect(
+            service.validateUser(userData.email, userData.password),
+        ).rejects.toThrow(new Error('Email ou senha incorreto(s) !'));
+
+        expect(service.validateUser).toHaveBeenCalledTimes(1);
+        expect(repository.findByEmail).toHaveBeenCalledWith(userData.email);
     });
 });
