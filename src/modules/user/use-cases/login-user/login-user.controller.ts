ef4ca64d44e7354ae7ea2e9e6a3c646219621d57
ocsoares <casoares016@gmail.com>
2023-02-04 21:23:21 -0300

    chore: remove comment

--- src/modules/user/use-cases/login-user/login-user.controller.ts
+++ src/modules/user/use-cases/login-user/login-user.controller.ts
@@ -19,9 +19,6 @@ export class LoginUserController implements IController {
     @HttpCode(HttpStatus.OK)
     @UseGuards(LocalAuthGuard)
     async handle(@Request() req: IAuthRequest): Promise<returnHandle> {
-        console.log('REQ USER:', req.user);
-
-        // o req.user.id NÃO está definido, então NÃO está passando para o JWT, porque o id tá vindo como _id, ARRUMAR ISSO !!!
         const JWT = await this._loginUserService.execute(req.user);
 
         return {
