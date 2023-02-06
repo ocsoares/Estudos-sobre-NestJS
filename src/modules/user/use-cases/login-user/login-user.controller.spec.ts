e4a66924677bcf33ee36e1ccf2dd72c8b44f55e8
ocsoares <casoares016@gmail.com>
2023-02-06 17:25:20 -0300

    test: add generate a jwt for a valid user test to login user controller test

--- src/modules/user/use-cases/login-user/login-user.controller.spec.ts
+++ src/modules/user/use-cases/login-user/login-user.controller.spec.ts
@@ -1,30 +1,127 @@
-import { JwtService } from '@nestjs/jwt';
+import { JwtService, JwtSignOptions } from '@nestjs/jwt';
 import { Test, TestingModule } from '@nestjs/testing';
+import { UserRepository } from '../../../../repositories/abstracts/UserRepository';
 import { LoginUserController } from './login-user.controller';
 import { LoginUserService } from './login-user.service';
+import * as supertest from 'supertest';
+import { INestApplication } from '@nestjs/common';
+import { PassportModule } from '@nestjs/passport';
+import { LocalStrategy } from '../../../../modules/auth/strategies/local.strategy';
+import { AuthService } from '../../../../modules/auth/auth.service';
+import { IUser } from 'src/models/IUser';
+import * as bcrypt from 'bcrypt';
+import * as jwt from 'jsonwebtoken';
+import { ConfigModule } from '@nestjs/config';
+import { IReturnUser } from 'src/interfaces/IReturnUser';
 
 describe('LoginUserController', () => {
-    let controller: LoginUserController;
+    let app: INestApplication;
+    let repository: UserRepository;
+    let service: LoginUserService;
+    let bcryptCompare: jest.SpyInstance;
 
+    const user: IUser = {
+        id: 'any_id',
+        name: 'Teste',
+        email: 'teste@gmail.com',
+        password: 'teste123',
+    };
+
+    const userNoPass: IReturnUser = {
+        id: user.id,
+        name: user.name,
+        email: user.email,
+    };
+
+    type dataLoginBody = { email: string; password: string };
+
+    const loginBody: dataLoginBody = {
+        email: 'teste@gmail.com',
+        password: 'teste123',
+    };
+
+    // Classe mockada com uma Chave Secreta, porque por algum motivo esse Método em LoginUserService NÃO
+    // estava ativando a Chave Secreta !!!
+    // OBS: Colocar essa Classe em useClass abaixo de provide: JwtService !!
+    class JwtServiceMock extends JwtService {
+        private readonly _secretKey = process.env.JWT_SECRET;
+
+        sign(
+            payload: string | object | Buffer,
+            options?: JwtSignOptions,
+        ): string {
+            return jwt.sign(payload, this._secretKey, options);
+        }
+    }
+
+    // Coloquei ConfigModule.forRoot() para ativar as Variáveis de Ambiente para esse teste, e NÃO passei
+    // NENHUM Parâmetro porque "isGlobal: true, envFilePath: '.env'," são DEFAULT no Método caso não passe
+    // nenhum Parâmetro !!!
     beforeEach(async () => {
         const module: TestingModule = await Test.createTestingModule({
+            imports: [
+                ConfigModule.forRoot(),
+                PassportModule.register({ defaultStrategy: 'local' }),
+            ],
             controllers: [LoginUserController],
             providers: [
                 LoginUserService,
                 {
                     provide: JwtService,
-                    useExisting: JwtService,
+                    useClass: JwtServiceMock, // Nos testes usar useClass ao invés de useExisting no testes do LoginService !!!
+                },
+                {
+                    provide: UserRepository,
                     useValue: {
-                        sign: jest.fn(),
+                        findByEmail: jest.fn(),
                     },
                 },
+                LocalStrategy,
+                AuthService,
             ],
         }).compile();
 
-        controller = module.get<LoginUserController>(LoginUserController);
+        app = module.createNestApplication();
+
+        repository = module.get<UserRepository>(UserRepository);
+        service = module.get<LoginUserService>(LoginUserService);
+
+        jest.spyOn(service, 'execute');
+        bcryptCompare = jest.spyOn(bcrypt, 'compare');
+
+        await app.init();
+    });
+
+    afterEach(async () => {
+        await app.close();
+        jest.clearAllMocks();
     });
 
     it('should be defined', () => {
-        expect(controller).toBeDefined();
+        expect(app).toBeDefined();
+        expect(repository).toBeDefined();
+        expect(service).toBeDefined();
+        expect(bcryptCompare).toBeDefined();
+    });
+
+    it('should generate a JWT for a valid user', async () => {
+        (repository.findByEmail as jest.Mock).mockResolvedValue(user);
+        bcryptCompare.mockResolvedValue(true);
+
+        const response = await supertest(app.getHttpServer())
+            .post('/auth/login')
+            .send(loginBody)
+            .expect(200);
+
+        const JWT = response.body.data;
+
+        expect(JWT).toEqual(expect.any(String));
+        expect(repository.findByEmail).toHaveBeenCalledWith(user.email);
+        expect(service.execute).toHaveBeenCalledTimes(1);
+        expect(service.execute).toHaveBeenCalledWith(userNoPass);
+        expect(bcryptCompare).toHaveBeenCalledWith(
+            loginBody.password,
+            user.password,
+        );
     });
 });
