0255c31716eeb3f6475f990bcbbf8e1bb3db0d50
ocsoares <casoares016@gmail.com>
2023-02-13 20:42:03 -0300

    feat: check if the id still exists in make transfer service

--- src/modules/transaction/use-cases/make-transfer/make-transfer.service.ts
+++ src/modules/transaction/use-cases/make-transfer/make-transfer.service.ts
@@ -1,8 +1,9 @@
 /* eslint-disable @typescript-eslint/naming-convention */
 
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { IService } from 'src/interfaces/IService';
 import { ITransaction } from 'src/models/ITransaction';
+import { UserRepository } from 'src/repositories/abstracts/UserRepository';
 import { TransactionRepository } from '../../../../repositories/abstracts/TransactionRepository';
 import { IReturnTransfer } from './interfaces/IReturnTransfer';
 
@@ -10,11 +11,20 @@ import { IReturnTransfer } from './interfaces/IReturnTransfer';
 export class MakeTransferService implements IService {
     constructor(
         private readonly _makeTransferRepository: TransactionRepository,
+        private readonly _userRepository: UserRepository,
     ) {}
 
     // Id vai ser colocado no Controller, com o ID armazenado no JWT !!
     async execute(data: ITransaction): Promise<IReturnTransfer> {
-        const { transfer_amount, card_number } = data;
+        const { transfer_amount, card_number, account_id } = data;
+
+        const userStillExists = await this._userRepository.findById(account_id);
+
+        if (!userStillExists) {
+            throw new BadRequestException(
+                'Não foi possível encontrar o usuário pelo id fornecido !',
+            );
+        }
 
         const fixTransferAmountTwoDecimalPlaces = Number(
             transfer_amount.toFixed(2),
