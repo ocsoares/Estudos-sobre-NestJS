c966ded1bc265ff03191ce0ed6cb26621030d96e
ocsoares <casoares016@gmail.com>
2023-02-13 17:57:50 -0300

    feat: add show all account transactions controller

--- src/modules/transaction/use-cases/show-all-account-transactions/show-all-account-transactions.controller.ts
+++ src/modules/transaction/use-cases/show-all-account-transactions/show-all-account-transactions.controller.ts
@@ -1,4 +1,23 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { IController, returnHandle } from 'src/interfaces/IController';
+import { ShowAllAccountTransactionsService } from './show-all-account-transactions.service';
+import { CurrentUser } from 'src/modules/auth/decorators/current-user.decorator';
+import { IReturnUser } from 'src/interfaces/IReturnUser';
 
 @Controller('show-all-account-transactions')
-export class ShowAllAccountTransactionsController {}
+export class ShowAllAccountTransactionsController implements IController {
+    constructor(
+        private readonly _showAllAccountTransactionsService: ShowAllAccountTransactionsService,
+    ) {}
+
+    @Get('transaction')
+    async handle(@CurrentUser() user: IReturnUser): Promise<returnHandle> {
+        const transactions =
+            await this._showAllAccountTransactionsService.execute(user.id);
+
+        return {
+            message: 'Suas transações foram encontradas com sucesso !',
+            data: transactions,
+        };
+    }
+}
