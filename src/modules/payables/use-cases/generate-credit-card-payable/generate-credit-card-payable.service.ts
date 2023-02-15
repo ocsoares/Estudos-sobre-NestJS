120f58b4139695736feb967b14940245043865c5
ocsoares <casoares016@gmail.com>
2023-02-14 21:23:27 -0300

    feat: add generate credit card payable service

--- src/modules/payables/use-cases/generate-credit-card-payable/generate-credit-card-payable.service.ts
+++ src/modules/payables/use-cases/generate-credit-card-payable/generate-credit-card-payable.service.ts
@@ -1,4 +1,6 @@
-import { Injectable } from '@nestjs/common';
+/* eslint-disable @typescript-eslint/naming-convention */
+
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { IService } from 'src/interfaces/IService';
 import { IPayable } from 'src/models/IPayable';
 import { PayablesRepository } from 'src/repositories/abstracts/PayablesRepository';
@@ -12,6 +14,36 @@ export class GenerateCreditCardPayableService implements IService {
     ) {}
 
     async execute(transfer_id: string): Promise<IPayable> {
-        return null;
+        const transfer = await this._transactionRepository.findOneById(
+            transfer_id,
+        );
+
+        if (!transfer_id) {
+            throw new BadRequestException('Id de transferência inválido !');
+        }
+
+        const currentDate = new Date();
+        const currentDateAfterThirtyDays = new Date(
+            currentDate.setDate(currentDate.getDate() + 30),
+        );
+
+        const threePercentProcessingFee = Number(
+            (
+                transfer.transfer_amount -
+                transfer.transfer_amount * 0.03
+            ).toFixed(2),
+        );
+
+        const makeCreditCardPayable =
+            await this._generateCreditCardPayableRepository.credit({
+                account_id: transfer.account_id,
+                transfer_amount: threePercentProcessingFee,
+                description: transfer.description,
+                status: 'waiting_funds',
+                payment_date: currentDateAfterThirtyDays,
+                transfer_id,
+            });
+
+        return makeCreditCardPayable;
     }
 }
