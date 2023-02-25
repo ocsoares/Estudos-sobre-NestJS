58e04d087379c5f3e843a2295679ceb7b69f26e6
ocsoares <casoares016@gmail.com>
2023-02-24 22:46:33 -0300

    chore: comment code

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,5 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { MongooseModule } from '@nestjs/mongoose';
 import { UserModule } from './modules/user/user.module';
 import { AuthModule } from './modules/auth/auth.module';
 import { TransactionModule } from './modules/transaction/transaction.module';
@@ -8,6 +7,7 @@ import { APP_GUARD } from '@nestjs/core';
 import { JwtAuthGuard } from './modules/auth/guards/jwt-auth.guard';
 import { PayablesModule } from './modules/payables/payables.module';
 import { LoginValidationBodyModule } from './modules/login-validation-body/login-validation-body.module';
+// import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
     imports: [
@@ -19,7 +19,7 @@ import { LoginValidationBodyModule } from './modules/login-validation-body/login
         // Conectassem no Banco de Dados REAL ao invés do In Memory !!!
         // OBS: Está comentado porque estou usando o Prisma, para testar a Implementação de OUTRO
         // Banco de Dados !!
-        MongooseModule.forRoot(process.env.ATLAS_URL_CONNECTION),
+        // MongooseModule.forRoot(process.env.ATLAS_URL_CONNECTION),
         UserModule,
         AuthModule,
         TransactionModule,
